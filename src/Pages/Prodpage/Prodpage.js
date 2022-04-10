9fe01f1e57ba2438b0e904359b101c30cbce25cd
Vhighc <eneh.victor@yahoo.com>
2022-04-10 21:38:09 +0100

    'Candleaf'

--- src/Pages/Prodpage/Prodpage.js
+++ src/Pages/Prodpage/Prodpage.js
@@ -4,58 +4,57 @@ import './Prodpage.css'
 const Prodpage = () => {
   return (
     <div className='page2prod'>
-        <div>
-            <div>
-                <figure>
+        <div className='propg2prod'>
+            <div className='prodpgsec01imgcont'>
                 <img src="./Images/Page2prodimg.svg" alt="" />
-                <figcaption>All hand-made with natural soy wax, Candleaf is made for your pleasure moments.</figcaption>
-                </figure>
             </div>
+            <p>All hand-made with natural soy wax, Candleaf is made for your pleasure moments.</p>
                 <span>FREE SHIPPING</span>
         </div>
-        <div>
+        <div className='presimica'>
             <h3>Spiced Mint Candleaf®</h3>
             <div className='simica'>
-                <div>
-                <h2>$ 9.99</h2>
-                <p>Quantity</p>  
-                <div className='prodpagebutt'>
+                <div className='subsimica01'>
+                  <h2>$ 9.99</h2>
+                  <p>Quantity</p>  
+                 <div className='prodpagebutt'>
                     <button>
-                    <div>+</div>
-                    <p>1</p> 
-                    <div>-</div>
+                      <div>+</div>
+                      <p>1</p> 
+                     <div>-</div>
                     </button>
-                </div>
+                 </div>
                 </div> 
-                <div>
+                <div className='prodpgradiosec'>
                     <div>
-                        <input type="radio" name='pay mode' />One time purchase
+                        <input type="radio" name='pay mode' />
+                        <label htmlFor="">One time purchase</label>
                     </div>
-                    <div>
-                        <input type="radio" name='pay mode' />Subscribe and delivery every 
+                    <div className='preradiosecoptpg'>
+                     <div className='radiosecoptpg'>
+                        <input type="radio" name='pay mode' />
+                        <label htmlFor="">Subscribe and delivery every </label>
                         <select>
                         <option value="4 weeks">4 weeks</option>
                         </select>
-                    </div>
-                    <div>
+                     </div>
+                     <div className='subsecoptpg'>
                         <p>Subscribe now and get the 10% of discount on every recurring order.  The discount will be applied at checkout. See details</p>
+                     </div>
                     </div>
+                  
                     <button>
                     <img src="./Images/Vectorcartbutt.svg" alt="" />
                     <p>+ Add to cart</p>
                     </button>
-                    <div>
-                        <p>Wax: Top grade Soy wax that delivers a smoke less,  consistent burn</p>
-                        <p>Fragrance: Premium quality ingredients with natural essential oils </p>
-                        <div>
-                        <p>Burning Time: 70-75 hours</p>
-                        <p>Dimension: 10cm x 5cm </p>
-                        <p>Weight: 400g </p>
-                        </div>
-                    </div>
                 </div>
-            </div>
-        </div>
+                </div>
+                  <div className='waxfragburn'>
+                    <p>Wax: Top grade Soy wax that delivers a smoke less,  consistent burn</p>
+                    <p>Fragrance: Premium quality ingredients with natural essential oils</p>
+                    <p>Burning Time: 70-75 hours Dimension: 10cm x 5cm Weight: 400g</p>
+                 </div>
+           </div>
     </div>
   )
 }
